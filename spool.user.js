e1a0acab1300c7546145e02a7f3e4a9933a487bb
James Elford <james.p.elford@gmail.com>
2014-03-15 18:40:44 +0000

    Updated .user.js to use latest commit id

--- spool.user.js
+++ spool.user.js
@@ -4,7 +4,7 @@
 // @name spool
 // @namespace com.jameselford.scripts
 // @require https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js
-// @require https://raw.github.com/jelford/spool/c2b2e73ef3b36cf424d910106daa2ba7dbc61488/spool.js
+// @require https://raw.github.com/jelford/spool/f2f623dcc63b8bec3205447db5d336f572eeea71/spool.js
 // @version 0.1.6_0
 // @grant none
 // ==/UserScript==
