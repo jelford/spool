96063bd45e7721727bb9b541c79461bc82b03c0f
James Elford <james.p.elford@gmail.com>
2014-03-15 17:25:34 +0000

    Updated spool.user.js to the latest commit, and added a guard that the user must be holding alt to activate spool

--- spool.user.js
+++ spool.user.js
@@ -4,8 +4,8 @@
 // @name spool
 // @namespace com.jameselford.scripts
 // @require https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js
-// @require https://raw.github.com/jelford/spool/28064b2c8d968d8c842774e3ccd5b13383c664f4/spool.js
-// @version 0.1.5_2
+// @require https://raw.github.com/jelford/spool/c2b2e73ef3b36cf424d910106daa2ba7dbc61488/spool.js
+// @version 0.1.6_0
 // @grant none
 // ==/UserScript==
 
@@ -13,8 +13,10 @@ this.$ = this.jQuery = jQuery.noConflict(true);
 
 $(document.body).click((function(spool) {
     return function(event) {
-        var target = $(event.target);
-        spool.read(target);
+        if (event.altKey) {
+            var target = $(event.target);
+            spool.read(target);
+        }
     };
 })(spool())
 );
