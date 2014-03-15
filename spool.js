c2b2e73ef3b36cf424d910106daa2ba7dbc61488
James Elford <james.p.elford@gmail.com>
2014-03-15 17:22:02 +0000

    Added delay for "tricky" words, tidied up wpm display with more options, and fixed position of text-display window after it\'s been initialized for a continuous spool session

--- spool.js
+++ spool.js
@@ -1,15 +1,23 @@
 function spool() {
     const DEFAULT_WPM = 200;
 
-    var spool = { };
-
-    var getUserPreference = function(label) {
+   var getUserPreference = function(label) {
         return window.localStorage.getItem(label);
     }
 
     var setUserPreference = function(key, value) {
         return window.localStorage.setItem(key, value);
-    }
+    };
+
+    var defaultWpm = function() {
+        if (getUserPreference('spool-wpm')) {
+            return getUserPreference('spool-wpm');
+        } else {
+            return DEFAULT_WPM;
+        }
+    };
+
+    var spool = { };
 
     var _workingPane = spool.workingPane = $('<div id="workingPane" class="spool-read-box">'); // give it the same class as the main readbox to ensure any font attributes are the same
     _workingPane.css('display', 'block');
@@ -33,19 +41,20 @@ function spool() {
             return pos;
         }
 
-        var offset = 0
+        var offset = 0;
 
-        for (var lookBack = 0; lookBack < 2 && pos - lookBack >= 0; ++lookBack) {
-            if ("acemnorsuvwxz".contains(word[pos-lookBack])) {
-                offset = lookBack;
-            } 
+        if (word.length > 7) {
+            offset = 1;
+        }
+        if (word.length > 10) {
+            offset = 2;
         }
 
-        offset += naturalPairAdjustment(pos, word, 'ou');
-        offset += naturalPairAdjustment(pos, word, 'ie');
-        offset += naturalPairAdjustment(pos, word, 'gh');
-        offset += naturalPairAdjustment(pos, word, 'ph');
-        offset += naturalPairAdjustment(pos, word, 'th');
+        offset += naturalPairAdjustment(pos - offset, word, 'ou');
+        offset += naturalPairAdjustment(pos - offset, word, 'ie');
+        offset += naturalPairAdjustment(pos - offset, word, 'gh');
+        offset += naturalPairAdjustment(pos - offset, word, 'ph');
+        offset += naturalPairAdjustment(pos - offset, word, 'th');
 
         return pos - offset;
     };
@@ -93,7 +102,7 @@ function spool() {
 
     var buildReadBox = function() {
         var style = $('<style type="text/css">')
-        style.html('.spool-read-box{font-size:2em;z-index:2;position:absolute;height:2.5em;width:10em;font-family:sans}.spool-read-box p{padding:0;display:inline}.spool-read-box .marker{height:.75em;padding:0;width:50%;border-right:1px solid red}.spool-read-box .container{width:100%;display:inline-block;background-color:#fff}.spool-read-box .controls{background-color:#fff;opacity:0;font-size:.5em;width:80%;padding-left:10%;padding-right:10%}.spool-read-box:hover .controls{opacity:1}.spool-read-box .controls .navigation-preview{padding:0 .1em}.spool-read-box .controls .control-group{display:inline-block;padding:1%}.spool-read-box .navigation-preview{font-size:.5em}.olp,.spool-read-box .controls .navigation-preview.on{color:red}');
+        style.html('.spool-read-box{font-size:2em;z-index:2;position:fixed;height:2.5em;width:10em;font-family:sans}.spool-read-box p{padding:0;display:inline}.spool-read-box .marker{height:.75em;padding:0;width:50%;border-right:1px solid red}.spool-read-box .container{width:100%;display:inline-block;background-color:#fff}.spool-read-box .controls{background-color:#fff;opacity:0;font-size:.5em;width:80%;padding-left:10%;padding-right:10%}.spool-read-box:hover .controls{opacity:1}.spool-read-box .controls .navigation-preview{padding:0 .1em}.spool-read-box .controls .control-group{display:inline-block;padding:1%}.spool-read-box .navigation-preview{font-size:.5em}.olp,.spool-read-box .controls .navigation-preview.on{color:red}');
         $(document.body).append(style);
 
         var controls = spool.controls = $('<div class="controls">');
@@ -112,6 +121,19 @@ function spool() {
         nextWordIndex: 0,
         words: [],
         intervalId: null,
+        increaseDelayForTrickyWord: function(word) {
+            var shouldDelay = false;
+
+            shouldDelay |= word === 'I';
+            var this_function = arguments.callee;
+            shouldDelay |= (this_function.previousWord && this_function.previousWord.length === word.length)
+            this_function.previousWord = word;
+
+            if (shouldDelay) {
+                this.pause();
+                window.setTimeout(this.resume.bind(this), spool.wordDuration/5);
+            }
+        },
         displayWord: function(word) {
 
             var previousTextHolder = this.contentArea.find('p').last();
@@ -130,6 +152,7 @@ function spool() {
 
             var word = this.words[this.nextWordIndex++];
             this.displayWord(word);
+            this.increaseDelayForTrickyWord(word);
 
             if (this.nextWordIndex >= this.words.length) {
                 this.finishedCallback();
@@ -186,7 +209,12 @@ function spool() {
     (function() {
         var wpmControls = $('<div class="control-group">');
         var wpmLabel = $('<span>wpm:</span>');
-        var wpmSetting = $('<select><option>default</option><option>100</option><option>200</option><option>250</option><option>300</option><option>350</option><option>400</option><option>450</option><option>500</option></select>')
+        var wpmSetting = $('<select>');
+
+        for (var i = 150; i < 800; i += 50) {
+            wpmSetting.append($('<option>').text(i).prop('selected', i == defaultWpm()));
+        }
+
         wpmControls.append(wpmLabel).append(wpmSetting);
         wpmSetting.change(function() {
             setWpm(wpmSetting.val());
@@ -216,7 +244,6 @@ function spool() {
         var offset = function(mouseEvent) {
             if (mouseEvent) {
                 var distance = Math.floor((navigationMouseEventData.startingX - mouseEvent.screenX) / 10);
-                console.log(distance);
                 return distance;
             } else {
                 return 0;
@@ -295,11 +322,7 @@ function spool() {
 
     var setWpm = function(wpm) {
         if (wpm === 'default') {
-            if (getUserPreference('spool-wpm')) {
-                wpm = getUserPreference('spool-wpm');
-            } else {
-                wpm = DEFAULT_WPM;
-            }
+            defaultWpm();
         }
         spool.wordDuration = (1000*60)/wpm;
         setUserPreference('spool-wpm', wpm);
@@ -348,14 +371,19 @@ function spool() {
         paragraphNode.fadeTo('fast', 0.5);
     }
 
-    var sanitizeText = (window.sane = function(text) {
+    var sanitizeText = window.sane = function(text) {
         var sane = text.trim();
         
+        // if a word appears twice, it won't display well. Insert a comma for readability
+        sane = sane.replace(/\b(\w+)\s+(\1)\b/g, '$1, $2');
+
         // get rid of dodgey double-hyphenation
         sane = sane.replace(/(\w+)(\W)(\2+)(\w+)/g, '$1 $2 $4');
+        sane = sane.replace(/([^\w\.])(\1+)/g, '$1 ');
         
         // split hypenated words
-        sane = sane.replace(/(\w+)([-:@])(\w+)/g, '$1$2 $3');
+        sane = sane.replace(/(\w)([-:@])(\w+)/g, '$1$2 $3');
+        sane = sane.replace(/(\w{2,})([^\w\s])(\w{2,})/g, '$1 $2 $3');
 
         // try to split long words according to natural rules
         sane = sane.replace(/(\w{7,})([aeiou]\w{3,})\b/g, '$1- $2');
@@ -364,7 +392,7 @@ function spool() {
         // split any remaining big words
         sane = sane.replace(/(\w{6,})(\w{5,})\b/g, '$1- $2');
         return sane;
-    });
+    };
 
     var ensureCoordinatesOnScreen = function(coords, margin) {
         var scrollBottom = $(window).scrollTop() + $(window).height();
@@ -374,7 +402,7 @@ function spool() {
         }
     };
 
-    var _readParagraph = spool.read = function(paragraphNode) {
+    var _readParagraph = spool.read = function(paragraphNode, dontMoveDisplay) {
 
         if (paragraphNode.closest(_readBox.contentArea).length > 0) {
             return; // don't accept requests targeted at our own content area
@@ -393,7 +421,9 @@ function spool() {
 
         ensureCoordinatesOnScreen(paragraphMid, _readBox.contentArea.height() * 1.1);
 
-        _readBox.show(paragraphMid);
+        if (!dontMoveDisplay) {
+            _readBox.show(paragraphMid);
+        }
 
         _readBox.start(
                 sanitizedText.split(/\s/g).filter(
@@ -403,7 +433,7 @@ function spool() {
                     if (spool.autoMoveNextParagraph) {
                         var nextParagraph = paragraphNode.next();
                         if (nextParagraph && nextParagraph.text()) {
-                            spool.read(nextParagraph);
+                            spool.read(nextParagraph, true);
                             return;
                         }
                     } 
@@ -412,7 +442,7 @@ function spool() {
                 });
     }
 
-    setWpm('default');
+    setWpm(defaultWpm());
 
     return spool;
 }
