a84fddef157a6974dcea0ca34df50d3db1fbd6ea
James Elford <james.p.elford@gmail.com>
2014-03-28 23:14:57 +0000

    Bumped version on .user.js

--- spool.user.js
+++ spool.user.js
@@ -4,8 +4,8 @@
 // @name spool
 // @namespace com.jameselford.scripts
 // @require https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
-// @require https://raw.github.com/jelford/spool/706aae2110bbe41eac7ac7e3ce9ef03a71a0533e/spool.js
-// @version 0.1.7_1
+// @require https://raw.github.com/jelford/spool/fd91c5ba84bf64be586d81c6806bc707f26e19b4/spool.js
+// @version 0.1.7_2
 // @grant none
 // ==/UserScript==
 
